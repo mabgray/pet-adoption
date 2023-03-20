a867a0bde450f5b6c91d93676d1e654ceae2bb7c
mab_gray <m2bbie@gmail.com>
2023-03-20 15:56:32 -0500

    Looped an array of arrays.Event Listener

--- js/script.js
+++ js/script.js
@@ -35,11 +35,35 @@ const francine = createPet("Francine","turtle");
 
 clover.sleep();
 baxter.play();
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+console.log(allPets);
+
+const showPets = function(petArray){
+    pets.innerHTML = "";
+
+    for(let pet of petArray){
+        let status = "ready to play";
+        if(pet.isTired >= 7){
+            status = "sleeping";
+        }
+         let li = document.createElement("li");
+         li.innerHTML = `<span class="pet-name">${pet.name} </span>the ${pet.species} is ${pet.status}`
+        pets.append(li);
+
+    }
+}
+statusButton.addEventListener("click", function(){
+        showPets(allPets);
+} );
+
 
 // console.log(clover.isTired);
 // console.log(baxter.isTired);
 
-// console.log(clover);
-// console.log(baxter);
+
 
 
